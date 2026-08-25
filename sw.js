7dc625cef162c04eab678a7f35ebec6460b5c812
ricmurtapsicologia <164411318+ricmurtapsicologia@users.noreply.github.com>
2026-08-25 18:37:44 -0300

    feat: adicionar cache offline basico

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='sono-em-dia-v1';
+const ASSETS=['./','./index.html','./css/styles.css','./js/content.js','./js/app.js','./assets/favicon.svg','./assets/social-preview.svg','./manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
